cd858af0c8f451d39e16737fe5a749e639f2f8de
Sebastian Alkstrand <Momonyaro@users.noreply.github.com>
2026-06-17 23:08:09 +0200

    added level_resource for holding level data, initial draft for a main menu and a basic save singleton for handling saving/loading

---
+++ starter_kit/level_utils/level_resource.gd
@@ -0,0 +1,14 @@
+class_name LevelResource extends Resource
+
+@export var _name: StringName
+@export var _scene: PackedScene
+@export var _tags: Array[String]
+
+func get_level_name() -> StringName:
+	return _name
+
+func get_scene() -> PackedScene:
+	return _scene
+
+func has_tag(tag: String) -> bool:
+	return _tags.has(tag)
---
+++ starter_kit/save_utils/save_autoload.gd
@@ -0,0 +1,100 @@
+extends Node
+
+# THIS FILE SHOULD BE ADDED TO THE PROJECT AS AN AUTOLOAD IN ORDER TO BE PROPERLY USED
+# THANK YOU FOR YOUR TIME!   - THE MANAGEMENT
+
+signal save_loaded(slot: int)
+
+const SAVE_PATH: String = "user://"
+const SAVE_FILE: String = "save"
+const SAVE_FILE_EXT: String = ".dat"
+
+const FINGERPRINT: String = "davey_jones"
+
+## If true, slot0.dat will attempt to load as the autoload is started
+const AUTOLOAD_ON_INIT: bool = true
+## How long (in seconds) before snapshot is written to disk after it is taken
+const SNAPSHOT_INTERVAL: float = 2.5
+
+var _save_data: Dictionary = {}
+var _snapshot_to_save: Dictionary = {}
+
+
+# ---- Godot Events ----
+
+func _enter_tree() -> void:
+	if AUTOLOAD_ON_INIT:
+		var err := load_slot(0)
+		if err != OK:
+			printerr("Failed to load save slot 0, received error code: %d" % err)
+
+func _process(_delta: float) -> void:
+	if _snapshot_to_save.size() > 0:
+		var current_time: float = Time.get_unix_time_from_system()
+		var timestamp: float = _snapshot_to_save.get("timestamp", current_time)
+		
+		if current_time - timestamp > SNAPSHOT_INTERVAL:
+			_write_to_disk(_snapshot_to_save.get("slot", 0), _snapshot_to_save.get("data", {}))
+			_snapshot_to_save = {}
+
+# ---- Public Functions ----
+
+func contains_data() -> bool:
+	return _save_data.keys().size() > 0
+
+func write(key: String, value: Variant) -> void:
+	# TODO: Change key parsing to treat it as path to place value correctly in the dictionary
+	_save_data[key] = value
+
+func read(key: String, default: Variant) -> Variant:
+	return _save_data.get(key, default)
+
+func save_slot(slot: int, now: bool = false) -> int:
+	if contains_data() == false:
+		return ERR_FILE_EOF
+
+	if now:
+		_write_to_disk(slot, _save_data)
+		_snapshot_to_save = {}
+		return OK
+
+	_snapshot_to_save = { "slot": slot, "data": _save_data, "timestamp": Time.get_unix_time_from_system() }
+	return OK
+
+func load_slot(slot: int) -> int:
+	var path: String = _get_save_slot_path(slot)
+	if FileAccess.file_exists(path) == false:
+		return ERR_FILE_NOT_FOUND
+
+	var file := FileAccess.open(path, FileAccess.READ)
+	var json_str: String = file.get_as_text()
+	var json_data: Dictionary = JSON.parse_string(json_str)
+	file.close()
+	
+	if json_data.get("f_print", "") != FINGERPRINT:
+		return ERR_INVALID_DATA
+	json_data.erase("f_print")
+
+	if contains_data():
+		print("Overriding loaded save data with incoming from slot %d" % slot)
+	
+	_save_data = json_data
+	save_loaded.emit(slot)
+	return OK
+
+# ---- Private Functions ----
+
+func _write_to_disk(slot: int, data: Dictionary) -> void:
+	var path: String = _get_save_slot_path(slot)
+	var file := FileAccess.open(path, FileAccess.WRITE)
+	
+	data["f_print"] = FINGERPRINT
+	var json_str := JSON.stringify(data, "\t")
+	
+	file.store_string(json_str)
+	file.close()
+
+## This will return a path to the save file for the given slot
+## example: _get_save_slot_path(0) -> "user://save0.dat"
+func _get_save_slot_path(slot: int) -> String:
+	return str("%s%s%d%s" % [SAVE_PATH, SAVE_FILE, slot, SAVE_FILE_EXT])
