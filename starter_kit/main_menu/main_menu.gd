31b6a6feef9efdd595f314d4eb8481cdbe3aa892
Sebastian Alkstrand <Momonyaro@users.noreply.github.com>
2026-06-19 11:14:53 +0200

    started outline of main menu

---
+++ starter_kit/main_menu/main_menu.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+# TODO: Add initial play button and use the level utility to allow for level selection
+# This will ultimately break the spearation that I want between our components 
+# but if I come up with something better I'll change it.
+
+# Otherwise, just make several versions of a main menu that layer in different functionality
+
+func _ready():
+	pass
